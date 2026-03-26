086a67683cd316d43f21535f21972a7bcb07b2e5
PARTH KAVAD <kavadparth54@gmail.com>
2026-03-27 01:33:59 +0530

    add vercel.json

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -2,7 +2,8 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      const uploadPath = process.env.NODE_ENV === 'production' || process.env.VERCEL ? "/tmp" : "./public/temp";
+      cb(null, uploadPath)
     },
     filename: function (req, file, cb) {
       
